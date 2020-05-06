98e344e3d91f2c5f1de25558dbb05722901d4024
Ananthan <ananthakrishnan1011@gmail.com>
2020-05-06 19:50:40 +0530

    UX improvements in Cart screen- *Button disabled if cart is empty *added progress indicator on placing order

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -24,33 +24,7 @@ class CartScreen extends StatelessWidget {
                   Text('Total'),
                   Spacer(),
                   Chip(label: Text('₹${cart.totalPrice}')),
-                  FlatButton(
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (ctx) => AlertDialog(
-                            title: Text('Do you want to place the order?'),
-                            actions: <Widget>[
-                              FlatButton(
-                                  onPressed: () {
-                                    Navigator.pop(ctx);
-                                  },
-                                  child: Text('No')),
-                              FlatButton(
-                                  onPressed: () {
-                                    Provider.of<Orders>(context, listen: false)
-                                        .addOrder(cart.items.values.toList(),
-                                            cart.totalPrice);
-                                    cart.clearCart();
-                                    Navigator.pop(ctx);
-                                    Scaffold.of(context).showSnackBar(SnackBar(content: Text('Order Placed!!'),));
-                                  },
-                                  child: Text('Yes')),
-                            ],
-                          ),
-                        );
-                      },
-                      child: Text('Order Now'))
+                  OrderNowButton(cart: cart)
                 ],
               ),
               padding: EdgeInsets.all(10),
@@ -73,5 +47,67 @@ class CartScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class OrderNowButton extends StatefulWidget {
+  const OrderNowButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  _OrderNowButtonState createState() => _OrderNowButtonState();
+}
+
+class _OrderNowButtonState extends State<OrderNowButton> {
+  var _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+        onPressed: (widget.cart.items.length == 0 || _isLoading)
+            ? null
+            : () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Do you want to place the order?'),
+                    actions: <Widget>[
+                      FlatButton(
+                          onPressed: () {
+                            Navigator.pop(ctx);
+                          },
+                          child: Text('No')),
+                      FlatButton(
+                          onPressed: () async {
+                            setState(() {
+                              _isLoading = true;
+                            });
+                            Navigator.pop(ctx);
+                            await Provider.of<Orders>(context, listen: false)
+                                .addOrder(widget.cart.items.values.toList(),
+                                    widget.cart.totalPrice);
+
+                            widget.cart.clearCart();
+                            setState(() {
+                              _isLoading = false;
+                            });
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                              content: Text('Order Placed!!'),
+                            ));
+
+                          },
+                          child: Text('Yes')),
+                    ],
+                  ),
+                );
+              },
+        child: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Text('Order Now'));
   }
 }
