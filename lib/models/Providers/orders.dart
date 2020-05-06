0e57024818b0bacbbc581afe32b2b327701c9799
Ananthan <ananthakrishnan1011@gmail.com>
2020-05-06 19:20:07 +0530

    uploading order to firebase

--- lib/models/Providers/orders.dart
+++ lib/models/Providers/orders.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:shoplite1/models/Providers/cart.dart';
+import 'package:http/http.dart' as http;
 
 class OrderItem {
   final String id;
@@ -17,14 +20,27 @@ class Orders with ChangeNotifier {
     return [..._items];
   }
 
-  void addOrder(List<CartItem> products, double total) {
+  void addOrder(List<CartItem> products, double total) async{
+    final url = 'https://shoplite-88df0.firebaseio.com/orders.json';
+    final timeStamp = DateTime.now();
+    var response = await http.post(url,body: json.encode({
+      'time' : timeStamp.toIso8601String(),
+      'total' : total,
+      'products' : products.map((product) =>{
+        'id' : product.prodId,
+        'title' : product.title,
+        'quantity' : product.quantity,
+        'price' : product.price
+      }).toList(),
+    }));
+
     _items.insert(
         0,
         OrderItem(
-            id: DateTime.now().toString(),
+            id: json.decode(response.body)['id'],//json.decode(response.body)['id'],
             products: products,
             total: total,
-            time: DateTime.now()));
+            time: timeStamp));
     notifyListeners();
   }
 }
